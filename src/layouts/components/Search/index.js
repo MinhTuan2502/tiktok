b3c64b937a03ccf589ebe37d8e428fbfd32431c9
MinhTuan2502 <TuanNTMSE160480@fpt.edu.vn>
2023-05-05 00:25:42 +0700

    upload source code

--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import HeadlessTippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 
-import * as searchServices from '~/apiServices/searchServices';
+import * as searchServices from '~/services/searchService';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccoutItem from '~/components/AccoutItem';
 import { useDebounce } from '~/hooks';
