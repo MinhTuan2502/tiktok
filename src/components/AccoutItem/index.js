d17ffb2ada3469f32a8121219d3cda52c0207b94
MinhTuan2502 <TuanNTMSE160480@fpt.edu.vn>
2023-05-06 15:17:25 +0700

    upload source code

--- src/components/AccoutItem/index.js
+++ src/components/AccoutItem/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
@@ -21,5 +22,9 @@ function AccoutItem({ data }) {
         </Link>
     );
 }
+
+AccoutItem.propTypes = {
+    data: PropTypes.object.isRequired,
+};
 
 export default AccoutItem;
