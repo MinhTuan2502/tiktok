b1dd38946226eaedf9da677c6349d84aead23062
MinhTuan2502 <TuanNTMSE160480@fpt.edu.vn>
2023-05-08 15:36:08 +0700

    upload source code

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,6 +4,8 @@ const routes = {
     profle: '/:nickname',
     upload: '/upload',
     search: '/search',
+    live: '/live',
+    explore: '/explore',
 };
 
 export default routes;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,12 +9,16 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
+import Explore from '~/pages/Explore';
 
 //Public routes
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
-    { path: config.routes.profle, component: Profile },
+    { path: config.routes.explore, component: Explore },
+    { path: config.routes.live, component: Live },
+    { path: config.routes.profile, component: Profile },
     { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
 ];
